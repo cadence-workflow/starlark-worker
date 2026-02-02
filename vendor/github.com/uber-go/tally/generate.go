c96454f20fe3537621f4320dcabeee934deb8be6
weric <weric@uber.com>
2026-02-02 11:54:22 -0800

    fix yaml

--- vendor/github.com/uber-go/tally/generate.go
+++
@@ -1,7 +0,0 @@
-package tally
-
-import (
-	_ "github.com/golang/mock/mockgen/model"
-)
-
-//go:generate mockgen -package tallymock -destination tallymock/stats_reporter.go -imports github.com/uber-go/tally github.com/uber-go/tally StatsReporter
